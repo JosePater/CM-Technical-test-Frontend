d6de0ea931b4771a41e96225aa0343a73e46fe9c
PaterDev <josepater_1130@hotmail.com>
2023-05-31 13:42:07 -0500

    feat: Component added: index, add, update

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { IndexComponent } from './Components/index/index.component';
+import { AddComponent } from './Components/add/add.component';
+import { UpdateComponent } from './Components/update/update.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    IndexComponent,
+    AddComponent,
+    UpdateComponent
   ],
   imports: [
     BrowserModule,
