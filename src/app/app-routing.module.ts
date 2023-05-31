37dff9efe0be464c0dee70cb9afacfd5eacb4fd7
PaterDev <josepater_1130@hotmail.com>
2023-05-31 14:32:06 -0500

    feat: Initial Routes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,16 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { IndexComponent } from './Components/index/index.component';
+import { AddComponent } from './Components/add/add.component';
+import { UpdateComponent } from './Components/update/update.component';
 
-const routes: Routes = [];
+// Asignación de rutas
+const routes: Routes = [
+  { path:'', redirectTo:'/index', pathMatch:'full' },
+  { path:'index', component: IndexComponent},
+  { path:'add', component: AddComponent },
+  { path:'update', component: UpdateComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
